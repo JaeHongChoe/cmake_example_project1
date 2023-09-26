73828f88de371892bf210b32093dbf7642e62328
JaeHongChoe <rkrkfkdi2@gmail.com>
2023-09-26 13:44:54 +0900

    added lala

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 auto main() -> int32_t {
     std::cout << "Hello World!" << std::endl;
     std::cout << "Example code for C++!" << std::endl;
+
+    std::cout << "Example code for C++ lalalala!" << std::endl;
     return 0;
 
 }
